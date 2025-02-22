b0b9991811867f6e11348bcd3c0ee63121bd3526
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-22 11:40:03 +0530

    [Om] Add. Made Rest Call and get Request Method and passed name as query parameter

--- Spring-MessagingApp/src/main/java/com/example/demo/controller/HelloController.java
+++ Spring-MessagingApp/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+	
+	//UseCase: 1
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz!!";
     }
+    
+    //UseCase: 2
+    @GetMapping("/query")
+    public String printHelloQuery(@RequestParam String name) {
+    	return "Hello" + name;
+    }
+    
+    
+    
+    
+    
 }
